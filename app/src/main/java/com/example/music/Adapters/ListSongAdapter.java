139b6a96c7a2b914ea51c5fa846fcdb60895bd9c
TuanAnh2401 <lta24012k2@gmail.com>
2024-01-05 22:25:42 +0700

    add phát ngẫu nhiên album

--- app/src/main/java/com/example/music/Adapters/ListSongAdapter.java
+++ app/src/main/java/com/example/music/Adapters/ListSongAdapter.java
@@ -1,5 +1,7 @@
 package com.example.music.Adapters;
 
+import android.content.Context;
+import android.content.Intent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -10,18 +12,20 @@ import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.bumptech.glide.Glide;
+import com.example.music.Activities.SongPlayerActivity;
 import com.example.music.Models.SongModel;
 import com.example.music.R;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class ListSongAdapter extends RecyclerView.Adapter<ListSongAdapter.ListSongViewHolder>{
     private List<SongModel> songList;
-
-    public ListSongAdapter(List<SongModel> songList) {
+    private Context mContext;
+    public ListSongAdapter(List<SongModel> songList, Context mContext) {
+        this.mContext = mContext;
         this.songList = songList;
     }
-
     public void setData(List<SongModel> list) {
         this.songList = list;
         notifyDataSetChanged();
@@ -46,6 +50,18 @@ public class ListSongAdapter extends RecyclerView.Adapter<ListSongAdapter.ListSo
 
         holder.txtNameSong.setText(song.getName());
         holder.txtSingerSong.setText(song.getSinger());
+        holder.itemView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int adapterPosition = holder.getAdapterPosition();
+                if (adapterPosition != RecyclerView.NO_POSITION) {
+                    Intent intent = new Intent(mContext, SongPlayerActivity.class);
+                    intent.putExtra("position", adapterPosition);
+                    intent.putParcelableArrayListExtra("songList", new ArrayList<>(songList));
+                    mContext.startActivity(intent);
+                }
+            }
+        });
     }
 
     @Override
--- app/src/main/java/com/example/music/Fragments/AlbumFragment.java
+++ app/src/main/java/com/example/music/Fragments/AlbumFragment.java
@@ -1,34 +1,36 @@
 package com.example.music.Fragments;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.TextView;
-import android.widget.ImageButton;
 
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.bumptech.glide.Glide;
+import com.example.music.Activities.SongPlayerActivity;
 import com.example.music.Adapters.ListSongAdapter;
 import com.example.music.Models.AlbumModel;
 import com.example.music.Models.SongModel;
 import com.example.music.R;
 
 import java.util.ArrayList;
+import java.util.Collections;
 
 public class AlbumFragment extends Fragment {
 
     private ImageView albumImage;
     private TextView albumName;
-
     private Button btnShuffle;
     private RecyclerView songRecyclerView;
-    private  ImageButton btnBack;
+    private ImageButton btnBack;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
@@ -44,6 +46,7 @@ public class AlbumFragment extends Fragment {
         if (bundle != null) {
             AlbumModel selectedAlbum = bundle.getParcelable("selectedAlbum");
             ArrayList<SongModel> allSongs = bundle.getParcelableArrayList("allSongs");
+
             if (allSongs != null && !allSongs.isEmpty()) {
                 loadAllAlbumsData(allSongs);
             }
@@ -52,30 +55,35 @@ public class AlbumFragment extends Fragment {
                 updateUI(selectedAlbum);
             }
         }
+
         btnBack.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 onBackPressed();
             }
         });
+
         return view;
     }
+
     private void onBackPressed() {
         if (getFragmentManager() != null) {
             getFragmentManager().popBackStack();
         }
     }
+
     private void loadAllAlbumsData(ArrayList<SongModel> allSongs) {
         btnShuffle.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                playRandomSong(allSongs);
             }
         });
 
         LinearLayoutManager layoutManager = new LinearLayoutManager(getContext());
         songRecyclerView.setLayoutManager(layoutManager);
 
-        ListSongAdapter listSongAdapter = new ListSongAdapter(allSongs);
+        ListSongAdapter listSongAdapter = new ListSongAdapter(allSongs, getContext());
         songRecyclerView.setAdapter(listSongAdapter);
     }
 
@@ -86,5 +94,15 @@ public class AlbumFragment extends Fragment {
                     .into(albumImage);
         }
         albumName.setText(selectedAlbum.getNameAlbum());
+    }
+
+    private void playRandomSong(ArrayList<SongModel> allSongs) {
+        if (allSongs != null && allSongs.size() > 0) {
+            Collections.shuffle(allSongs);
+            Intent intent = new Intent(getContext(), SongPlayerActivity.class);
+            intent.putParcelableArrayListExtra("songList", allSongs);
+            intent.putExtra("position", 0);
+            startActivity(intent);
+        }
     }
 }
